a322ce7687d74459fb06f5768fc94b07146cf152
Wei TingTao <s952865@go.thu.edu.tw>
2015-12-11 15:29:24 -0600

    Initial Commit

---
+++ UIDynamicAnimator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIDynamicAnimator
+//
+//  Created by Wei TingTao on 12/11/15.
+//  Copyright © 2015 weitingtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIDynamicAnimator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIDynamicAnimator
+//
+//  Created by Wei TingTao on 12/11/15.
+//  Copyright © 2015 weitingtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
